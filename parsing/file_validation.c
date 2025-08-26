0405f16296ddf2286415571083c1fe94d14e07f1
Anas <ataai@e3r11p4.1337.ma>
2025-08-26 16:06:58 +0100

    fixing norm on parsing/

--- parsing/file_validation.c
+++ parsing/file_validation.c
@@ -12,16 +12,6 @@
 
 #include "../cub3D.h"
 
-void	extension_err(void)
-{
-	ft_putstr_fd("error: invalid file extension!\n", 2);
-}
-
-void	cant_open_err(void)
-{
-	ft_putstr_fd("error: can't open the file!\n", 2);
-}
-
 int	validate_file_extension(char *name)
 {
 	int	i;
@@ -39,230 +29,22 @@ int	validate_file_extension(char *name)
 	return (0);
 }
 
-void	init_cube(t_cube *cube)
-{
-	cube->no = NULL;
-	cube->so = NULL;
-	cube->we = NULL;
-	cube->ea = NULL;
-	cube->c = -1;
-	cube->f = -1;
-	cube->map = NULL;
-}
-
-int	set_no_so(t_cube *cube, char *line, int *n)
-{
-	if (!ft_strncmp(line, "NO ", 3))
-	{
-		if (cube->no != NULL)
-			return (1);
-		cube->no = get_rest_line(line, 1);
-		if (cube->no == NULL)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	if (!ft_strncmp(line, "SO ", 3))
-	{
-		if (cube->so != NULL)
-			return (1);
-		cube->so = get_rest_line(line, 1);
-		if (cube->so == NULL)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	return (2);
-}
-
-int	set_we_ea(t_cube *cube, char *line, int *n)
-{
-	if (!ft_strncmp(line, "WE ", 3))
-	{
-		if (cube->we != NULL)
-			return (1);
-		cube->we = get_rest_line(line, 1);
-		if (cube->we == NULL)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	if (!ft_strncmp(line, "EA ", 3))
-	{
-		if (cube->ea != NULL)
-			return (1);
-		cube->ea = get_rest_line(line, 1);
-		if (cube->ea == NULL)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	return (2);
-}
-
-int	set_f_c(t_cube *cube, char *line, int *n)
-{
-	if (!ft_strncmp(line, "F ", 2))
-	{
-		if (cube->f != -1)
-			return (1);
-		cube->f = get_color(line);
-		printf("heeeereeee =====> %u\n", cube->f);
-		if (cube->f == -1)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	if (!ft_strncmp(line, "C ", 2))
-	{
-		if (cube->c != -1)
-			return (1);
-		cube->c = get_color(line);
-		if (cube->c == -1)
-			return (1);
-		*n = *n - 1;
-		return (0);
-	}
-	return (2);
-}
-
-int	is_all_space(char *line)
-{
-	int	i;
-
-	i = 0;
-	while(line[i])
-	{
-		if (line[i] != ' ')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-void	free_tab(char **tab)
-{
-	int	i;
-
-	if (tab == NULL)
-		return ;
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-}
-
-char	*rm_newline(char *str)
-{
-	int	i;
-
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			str[i] = '\0';
-		i++;
-	}
-	return(str);
-}
-
-long	convert_rgb(char **section)
-{
-	int		i;
-	int		j;
-	int r;
-	int g;
-	int b;
-
-	i = 0;
-	while (section[i])
-	{
-		j = 0;
-		if (ft_strlen(section[i]) > 3 || (ft_strlen(section[i]) == 3 && ft_strcmp(section[i], "255") > 0))
-			return (free_tab(section), -1);
-		while (section[i][j])
-		{
-			if (!ft_isdigit(section[i][j]))
-				return (free_tab(section), -1);
-			j++;
-		}
-		i++;
-	}
-	r = ft_atoi(section[0]);
-	g = ft_atoi(section[1]);
-	b = ft_atoi(section[2]);
-	return (free_tab(section), ((r << 16) | (g << 8) | b));
-}
-
-long	get_color(char *line)
-{
-	char	*l;
-	char	**sections;
-
-	l = get_rest_line(line, 0);
-	if (l == NULL)
-		return (-1);
-	sections = ft_split(l, ',');
-	if (sections == NULL)
-		return (-1);
-	if (!sections[0] || !sections[1] || !sections[2] || sections[3])
-	{
-		free_tab(sections);
-		return (-1);
-	}
-	return (convert_rgb(sections));
-}
-
-char	*get_rest_line(char *line, int flag)
-{
-	char **sections;
-	char	*rest;
-	int		tmp;
-
-	sections = ft_split(line, ' ');
-	if (sections == NULL || sections[1] == NULL || sections[2] != NULL)
-	{
-		free_tab(sections);
-		return (NULL);
-	}
-	rest = sections[1];
-	free(sections[0]);
-	free(sections);
-	if (flag)
-	{
-		tmp = open(rest, O_RDONLY, 0444);
-		if (tmp == -1)
-		{
-			free(rest);
-			ft_putstr_fd("error: can't find texture\n", 2);
-			return (NULL);
-		}
-		close(tmp);
-	}
-	return (rest);
-}
-
 int	set_param(t_cube *cube, char *line, int *n)
 {
 	if (!ft_strncmp(line, "NO ", 3) || !ft_strncmp(line, "SO ", 3))
 	{
-			if (set_no_so(cube, line, n) == 1)
-				return (1);
+		if (set_no_so(cube, line, n) == 1)
+			return (1);
 	}
 	else if (!ft_strncmp(line, "WE ", 3) || !ft_strncmp(line, "EA ", 3))
 	{
-			if (set_we_ea(cube, line, n) == 1)
-				return (1);
+		if (set_we_ea(cube, line, n) == 1)
+			return (1);
 	}
 	else if (!ft_strncmp(line, "F ", 2) || !ft_strncmp(line, "C ", 2))
 	{
-			if (set_f_c(cube, line, n) == 1)
-				return (1);
+		if (set_f_c(cube, line, n) == 1)
+			return (1);
 	}
 	else if (is_all_space(line))
 		return (0);
@@ -288,16 +70,6 @@ int	param_check(t_cube *cube, int fd)
 		free(line);
 	}
 	return (0);
-}
-
-void	free_cube(t_cube *cube)
-{
-	free(cube->no);
-	free(cube->so);
-	free(cube->we);
-	free(cube->ea);
-	free_tab(cube->map);
-	free(cube);
 }
 
 t_cube	*validate_file(int fd)
--- parsing/parse_map.c
+++ parsing/parse_map.c
@@ -1,55 +1,5 @@
 #include "../cub3D.h"
 
-int	tab_len(char **tab)
-{
-	int	i;
-
-	if (tab == NULL)
-		return (0);
-	i = 0;
-	while (tab[i])
-		i++;
-	return (i);
-}
-
-int	tab_join(t_cube *cube, char *line)
-{
-	char	**new_map;
-	int		i;
-
-	if (line == NULL)
-		return (-1);
-	new_map = malloc(sizeof(char *) * (tab_len(cube->map) + 2));
-	if (new_map == NULL)
-		return (-1);
-	if (cube->map == NULL)
-	{
-		cube->map = new_map;
-		cube->map[0] = line;
-		cube->map[1] = NULL;
-		return (0);
-	}
-	i = 0;
-	while (cube->map[i])
-	{
-		new_map[i] = cube->map[i];
-		i++;
-	}
-	new_map[i] = line;
-	new_map[i + 1] = NULL;
-	free(cube->map);
-	cube->map = new_map;
-	return (0);
-}
-
-int	banned_char(char c)
-{
-	if (c != ' ' && c != '1' && c != '0' && c != 'N'
-			&& c != 'S' && c != 'E' && c != 'W')
-		return (1);
-	return (0);
-}
-
 int	check_map_line(char *line)
 {
 	int	i;
@@ -95,20 +45,13 @@ int	parse_map(t_cube *cube, int fd)
 			continue ;
 		}
 		if (player_in_line(line, &player) > 1)
-				return (free(line), 1);
+			return (free(line), 1);
 		if (check_map_line(line))
 			return (free(line), 1);
 		if (tab_join(cube, line))
 			return (free(line), 1);
 	}
 	if (player == 0)
-		return (1);
-	return (0);
-}
-
-int	is_valid_stop(char c)
-{
-	if (c == '0' || c == '1' || is_player(c))
 		return (1);
 	return (0);
 }
@@ -122,84 +65,13 @@ int	check_surroundings(t_cube *cube, char *line, int x, int y)
 		return (1);
 	if (x == len - 1)
 		return (1);
-	if ((int)ft_strlen(cube->map[y - 1]) - 1 < x || !is_valid_stop(cube->map[y - 1][x]))
+	if ((int)ft_strlen(cube->map[y - 1]) - 1 < x
+		|| !is_valid_stop(cube->map[y - 1][x]))
 		return (1);
-	if ((int)ft_strlen(cube->map[y + 1]) - 1 < x || !is_valid_stop(cube->map[y + 1][x]))
+	if ((int)ft_strlen(cube->map[y + 1]) - 1 < x
+		|| !is_valid_stop(cube->map[y + 1][x]))
 		return (1);
 	if (!is_valid_stop(line[x - 1]) || !is_valid_stop(line[x + 1]))
 		return (1);
-	return (0);
-}
-
-int	is_player(char c)
-{
-	if (c == 'N' || c == 'W' || c == 'E' || c == 'S')
-		return (1);
-	return (0);
-}
-
-int	first_last_line(t_cube *cube)
-{
-	int	i;
-	char	*line;
-
-	i = 0;
-	line = cube->map[0];
-	while (line[i])
-	{
-		if (line[i] != '1' && line[i] != ' ')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	mid_line(t_cube *cube, int y)
-{
-	int	i;
-	int	len;
-	char	*line;
-
-
-	i = 0;
-	line = cube->map[y];
-	len = ft_strlen(line);
-	while (line[i])
-	{
-		if (line[i] == '0' || is_player(line[i]))
-		{
-			if (check_surroundings(cube, line, i, y))
-				return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-int	map_check(t_cube *cube)
-{
-	int	i;
-	int	len;
-
-	if (cube->map == NULL)
-		return (1);
-	len = tab_len(cube->map);
-	if (len < 3)
-		return (1);
-	i = 0;
-	while (cube->map[i])
-	{
-		if (i == 0 || i == len - 1)
-		{
-			if (first_last_line(cube))
-				return (1);
-		}
-		else
-		{
-			if (mid_line(cube, i))
-				return (1);
-		}
-		i++;
-	}
 	return (0);
 }
